32c0ea5e5ea5f1a59da528b6ce5e84a1e33a9e6a
kamalpuvvada <kamalpuvvada@gmail.com>
2025-09-22 17:20:32 +0530

    commiting some code

--- functions-api/upload-photo/upload-photo.js
+++ functions-api/upload-photo/upload-photo.js
@@ -4,7 +4,7 @@ const { type } = require('@testing-library/user-event/dist/type');
 const multipart = require('parse-multipart-data');
 const { BlobServiceClient } = require('@azure/storage-blob');
 const { CosmosClient } = require('@azure/cosmos');
-const { upload } = require('@testing-library/user-event/dist/upload');
+// const { upload } = require('@testing-library/user-event/dist/upload');
 
 app.http('upload-photo', {
     methods: ['GET', 'POST'],
